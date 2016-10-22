c98edf3fc1248244d767da144f6b55b5ee63242d
Bruno Paulo <bruno@werules.com>
2016-10-22 07:53:10 -0200

    running new generic store bot with fabric

--- sample/app.py
+++ sample/app.py
@@ -1,3 +1,4 @@
+import run_generic_store
 import telepot
 from telepot.delegate import pave_event_space, per_chat_id, create_open
 import time
@@ -33,6 +34,15 @@ class CreateShopBot(telepot.helper.ChatHandler):
             elif self.telegram_api_key == '':
                 self.telegram_api_key = text
                 text = 'Done! Let me create your bot for you, give me a minute...'
+                success = run_generic_store.create_new_store(bot_name=self.telegram_api_key.split(':')[0],
+                                                             shopify_api_key=self.shopify_api_key,
+                                                             shopify_api_password=self.shopify_api_password,
+                                                             shopify_hostname=self.shopify_hostname,
+                                                             telegram_api_key=self.telegram_api_key)
+                if success:
+                    text = "Hoorray! You're new telegram store is online and running!"
+                else:
+                    text = "Ops, something went wrong. Please try again :("
 
             if text is not None:
                 bot.sendMessage(chat_id=chat_id, text=text)
