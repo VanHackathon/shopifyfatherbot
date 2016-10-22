fdffe76cc446f587d8956ffbcb1bb7b439f871d4
Bruno Paulo <bruno@werules.com>
2016-10-22 15:21:36 -0200

    first version of shop delete

--- sample/app.py
+++ sample/app.py
@@ -1,17 +1,26 @@
+import copy
 import run_generic_store
 import telepot
 from telepot.delegate import pave_event_space, per_chat_id, create_open
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 import time
 import sys
+
+
+class Shop(object):
+    def __init__(self):
+        self.shopify_api_key = ''
+        self.shopify_api_password = ''
+        self.shopify_hostname = ''
+        self.telegram_api_key = ''
 
 
 class CreateShopBot(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
-        self.shopify_api_key = ''
-        self.shopify_api_password = ''
-        self.shopify_hostname = ''
-        self.telegram_api_key = ''
+        self.list_shops = []
+        self.temp_bot = Shop()
+        self.shop_names = []
 
     def on_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
@@ -20,19 +29,30 @@ class CreateShopBot(telepot.helper.ChatHandler):
         if chat_type == 'private' and content_type == 'text':
             text = msg['text']
             if text == '/create' or text == '/start':   #user may be restarting flow
-                self.clear_attributes()
+                self.temp_bot = Shop()
                 text = 'Great! Please send me your Shopify API key'
+            elif text == '/delete':
+                bot.sendMessage(chat_id=chat_id,
+                                text='Which shop would you like to delete?',
+                                reply_markup=self.get_shops_keyboard())
+            elif text in self.shop_names:
+                self.shop_names.remove(text)
+                for shop in self.list_shops:
+                    if shop.shopify_hostname == text:
+                        self.list_shops.remove(shop)
+                        break
+                #TODO matar processo e apagar pasta
             elif self.shopify_api_key == '':
-                self.shopify_api_key = text
+                self.temp_bot.shopify_api_key = text
                 text = 'Great! Now send me your Shopify API password'
-            elif self.shopify_api_password == '':
+            elif self.temp_bot.shopify_api_password == '':
                 self.shopify_api_password = text
                 text = 'Great! Now send me your Shopify hostname'
-            elif self.shopify_hostname == '':
+            elif self.temp_bot.shopify_hostname == '':
                 self.shopify_hostname = text
                 text = 'Great! Now send me your Telegram bot key'
             elif self.telegram_api_key == '':
-                self.telegram_api_key = text
+                self.temp_bot.telegram_api_key = text
                 text = 'Done! Let me create your bot for you, give me a minute...'
                 success = run_generic_store.create_new_store(bot_name=self.telegram_api_key.split(':')[0],
                                                              shopify_api_key=self.shopify_api_key,
@@ -40,6 +60,9 @@ class CreateShopBot(telepot.helper.ChatHandler):
                                                              shopify_hostname=self.shopify_hostname,
                                                              telegram_api_key=self.telegram_api_key)
                 if success:
+                    self.list_shops.append(copy.copy(self.temp_bot))
+                    self.shop_names.append(self.temp_bot.shopify_hostname)
+                    self.temp_bot = Shop()
                     text = "Hoorray! You're new telegram store is online and running!"
                 else:
                     text = "Ops, something went wrong. Please try again :("
@@ -47,11 +70,13 @@ class CreateShopBot(telepot.helper.ChatHandler):
             if text is not None:
                 bot.sendMessage(chat_id=chat_id, text=text)
 
-    def clear_attributes(self):
-        self.shopify_api_key = ''
-        self.shopify_api_password = ''
-        self.shopify_hostname = ''
-        self.telegram_api_key = ''
+    def get_shops_keyboard(self):
+        keyboard = []
+
+        for shop in self.list_shops:
+            keyboard.append([KeyboardButton(text=shop.shopify_hostname)])
+
+        return ReplyKeyboardMarkup(inline_keyboard=keyboard)
 
 
 TOKEN = sys.argv[1]
